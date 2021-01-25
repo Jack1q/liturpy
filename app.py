452557445517c8e0356e73ef8a2564b6328ca167
Jack1q <johndonofrio2021@students.sjcadets.org>
2021-01-24 21:47:13 -0500

    developed website a bit more

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template, jsonify
 from liturpy import LiturgicalCalendar
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+
+
+@app.route('/home')
+def main():
+    return render_template('page.html')
 
 @app.route('/')
-def main():
-    return jsonify(LiturgicalCalendar().get_main_calendar())
+def landing_page():
+    return main()
+
+# @app.route('/')
+# def main():
+#     return jsonify(LiturgicalCalendar().get_main_calendar())
+
+@app.route('/calendar')
+def calendar_page():
+    return render_template('calendar.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
 
 @app.route('/<int:year>')
 def get_year_calendar(year):
